74a3780aea3c963353b1699cd5840cff5b18d49c
Lucas Maume <lmaume@k0r2p4.42mulhouse.fr>
2023-10-31 16:10:37 +0100

    memcpy correction

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: lmaume <lmaume@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 12:00:29 by lmaume            #+#    #+#             */
-/*   Updated: 2023/10/31 13:00:49 by lmaume           ###   ########.fr       */
+/*   Updated: 2023/10/31 15:52:09 by lmaume           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (!dest || !src)
-		return (0);
 	while (i < n)
 	{
-		((char *)dest)[i] = ((const char *)src)[i];
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
 		i++;
 	}
-	((char *)dest)[i] = '\0';
 	return (dest);
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	size_t	n = 12;
-	char	src[] = "blablablabloooooo";
-	char	dest[] = "une sting";
-
-	ft_memcpy(dest, src, n);
-	printf("%s\n", dest);
-	return (0);
 }

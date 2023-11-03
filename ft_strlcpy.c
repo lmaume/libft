28af47c98faa6f25dd4e2b86cd4b3870e192c90d
Lucas Maume <lmaume@k0r3p3.42mulhouse.fr>
2023-11-03 17:54:18 +0100

    cpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,39 @@
+
+#include <stddef.h>
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size > (size_t)ft_strlen(src))
+	{
+		while (src[i+1] != '\0' && i < size-1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		while (src[i] != '\0' && i < size-1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+	}
+	return (ft_strlen(src));
+}
+
+#include <stdio.h>
+#include <bsd/string.h>
+
+int	main(void)
+{
+	char	src[] = "quoi?ggg";
+	char	dst1[] = "coubeh";
+	char	dst2[] = "coubeh";
+	size_t	size = 17;
+
+	printf("Vraie fonction; \n%ld\n\n", strlcpy(dst1, src, size));
+	printf("Ma fonction   ; \n%ld\n", ft_strlcpy(dst2, src, size));
+	printf("\n%s\n%s\n", dst1, dst2);
+}

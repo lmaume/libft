92903be31c6b155290f29125e26a1fd71a721c3e
Lucas Maume <lmaume@k1r3p7.42mulhouse.fr>
2023-12-06 19:26:44 +0100

    fd's, putnbr a finir

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: lmaume <lmaume@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 17:02:16 by lmaume            #+#    #+#             */
-/*   Updated: 2023/12/06 17:38:14 by lmaume           ###   ########.fr       */
+/*   Updated: 2023/12/06 19:26:32 by lmaume           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,33 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	char	*c;
 	int		i;
+	int		sign;
 
 	i = 0;
+	sign = 1;
 	c = NULL;
-	if (n < 10)
-		c[i] = n + '0';
-	while (n >= 10)
+	if (n < 0)
+		sign *= -1;
+	if (n / 10 != 0)
 	{
-		c[i] = (n / 10 + '0');
+		c[i] = (n % 10) + '0';
 		i++;
-		c[i] = (n % 10 + '0');
+		n /= 10;
+		ft_putnbr_fd(n, fd);
 	}
-	ft_putstr_fd(c, fd);
+	if (sign == -1)
+		ft_putchar_fd('-', fd);
+	while (i > 0)
+	{
+		ft_putchar_fd(c[i], fd);
+		i--;
+	}
 }
 
-int main(void)
-{
-	int fd = 1;
-	int n = 18;
-	ft_putnbr_fd(n, fd);
-}
+// int	main(void)
+// {
+// 	int	fd = 1;
+// 	int	n = 18;
+
+// 	ft_putnbr_fd(n, fd);
+// }

6d5d0cd19105079f9eb302c51469a39e79d36d99
Lucas Maume <lmaume@k1r3p8.42mulhouse.fr>
2023-12-11 18:32:21 +0100

    itoa's done, only split's missing

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,41 +6,61 @@
 /*   By: lmaume <lmaume@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 17:04:18 by lmaume            #+#    #+#             */
-/*   Updated: 2023/12/08 19:11:21 by lmaume           ###   ########.fr       */
+/*   Updated: 2023/12/11 18:31:26 by lmaume           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
+static
+int	getsign(int number)
+{
+	if (number < 0)
+		return (1);
+	return (0);
+}
+
+static
+int	getlen(int number)
+{	
+	int	len;
+
+	len = 0;
+	if (number == 0)
+		return (1);
+	while (number != 0)
+	{
+		len++;
+		number /= 10;
+	}
+	return (len);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;
-	int		temp;
 	int		sign;
 	int		len;
 
-	i = 0;
-	sign = 0;
-	while (temp != 0)
-	{
-		len++;
-		temp /= 10;
-	}
-	if (n < 0)
-	{
-		if (n == -2147483648)
-			return (ft_strdup("-2147483648"));
-		sign = 1;
-		str[0] = '-';
-		n *= -1;
-	}
+	len = getlen(n);
+	sign = getsign(n);
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
 	str = ft_calloc((len + sign) + 1, 1);
 	if (!str)
 		return (NULL);
+	if (sign == 1)
+	{
+		str[0] = '-';
+		n *= -1;
+	}
 	while (len >= sign)
 	{
-		str[len] = (n % 10) + '0';
+		if (len - (1 - sign) >= 0)
+			str[len - (1 - sign)] = (n % 10) + '0';
+		n /= 10;
+		len--;
 	}
 	return (str);
 }

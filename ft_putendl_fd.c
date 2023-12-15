782dfaf66d8628eb0ea36f3cb45bdbb4b95d1205
Lucas Maume <lmaume@k2r1p2.42mulhouse.fr>
2023-12-15 18:32:13 +0100

    all done v8

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: lmaume <lmaume@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 16:49:48 by lmaume            #+#    #+#             */
-/*   Updated: 2023/12/06 17:01:02 by lmaume           ###   ########.fr       */
+/*   Updated: 2023/12/15 18:29:47 by lmaume           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 void	ft_putendl_fd(char *s, int fd)
 {
 	if (fd > 0 && s)
+	{
 		ft_putstr_fd(s, fd);
-	ft_putchar_fd('\n', fd);
+		ft_putchar_fd('\n', fd);
+	}
 }
